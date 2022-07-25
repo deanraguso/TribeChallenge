77e8c268d8b4691390d6c45703af5644045179a0
Dean Raguso <deanraguso@gmail.com>
2022-07-25 23:16:58 +1000

    feat: add order item-wise validation

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -9,5 +9,18 @@ RSpec.describe ::Order, type: :class do
         expect { described_class.new(input: "asdf 12342134 asdf") }.to raise_error(ArgumentError)
       end
     end
+
+    context "when a quantity isn't a positive integer" do
+      it 'raises an argument error' do
+        expect { described_class.new(input: "-5 IMG") }.to raise_error(ArgumentError, "-5 is not an integer greater than 0!")
+        expect { described_class.new(input: "27.5 IMG") }.to raise_error(ArgumentError, "27.5 is not an integer greater than 0!")
+      end
+    end
+
+    context "when the format isn't valid" do
+      it 'raises an argument error' do
+        expect { described_class.new(input: "5 ARTICLE") }.to raise_error(ArgumentError, "ARTICLE is not a valid format!")
+      end
+    end
   end
 end
--- src/models/order.rb
+++ src/models/order.rb
@@ -1,8 +1,12 @@
+require_relative "./post.rb"
+
 class Order
   attr_reader :posts
 
   def initialize(input:)
-    validate_input!
+    validate_input!(input: input)
+
+    @posts = get_posts(input: input)
   end
 
   private
@@ -10,11 +14,29 @@ class Order
   def validate_input!(input:)
     input_array = input.split(" ")
 
-    unless ((input_array.length > 0) && ((input_array % 2) == 0))
+    unless ((input_array.length > 0) && ((input_array.length % 2) == 0))
       raise ArgumentError.new("input is not of the correct format!")
     end
   end
 
   def get_posts(input:)
+    posts = []
+
+    input_array = input.split(" ")
+    while (post_order = input_array.slice!(-2, 2))
+      validate_post_order!(quantity: post_order[0], format: post_order[1])
+    end
+
+    posts
+  end
+
+  def validate_post_order!(quantity:, format:)
+    unless ((quantity.to_i.to_s == quantity) && (quantity.to_i > 0))
+      raise ArgumentError.new("#{quantity} is not an integer greater than 0!")
+    end
+
+    unless Post::FORMATS.values.include?(format.to_sym)
+      raise ArgumentError.new("#{format} is not a valid format!")
+    end
   end
 end
