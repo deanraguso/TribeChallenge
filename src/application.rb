7f268fb6976f36d7e34082a43ce7a972baeee8a6
Dean Raguso <deanraguso@gmail.com>
2022-07-26 01:01:18 +1000

    feat: add application entry point

--- src/application.rb
+++ src/application.rb
@@ -1,2 +1,10 @@
 require_relative "models/post.rb"
 require_relative "models/order.rb"
+
+puts "Enter your order below:"
+input = gets.chomp
+
+order = Order.new(input: input)
+
+puts "Your order cost breakdown:"
+order.print_cost_breakdown
